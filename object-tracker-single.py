0d1b2d94582c4bc0a3c44c1ec5817cbc9f40b040
iw4p <nimakbarzade@yahoo.com>
2019-11-26 19:35:49 +0330

    update to python3, fix bugs

--- object-tracker-single.py
+++ object-tracker-single.py
@@ -3,22 +3,32 @@ import dlib
 import cv2
 import argparse as ap
 import get_points
+# import cv2
+print(cv2.__version__)
+# x markz Box 
+# y vstsh 
+# width box 
+# hight box
 
 def run(source=0, dispLoc=False):
+
+
     # Create the VideoCapture object
     cam = cv2.VideoCapture(source)
-
+    # success,image = cam.read()
+    cam.set(3,640)
+    cam.set(4,480)
     # If Camera Device is not opened, exit the program
     if not cam.isOpened():
-        print "Video device or file couldn't be opened"
+        print("Video device or file couldn't be opened")
         exit()
     
-    print "Press key `p` to pause the video to start tracking"
+    print("Press key `p` to pause the video to start tracking")
     while True:
         # Retrieve an image and Display it.
         retval, img = cam.read()
         if not retval:
-            print "Cannot capture frame device"
+            print("Cannot capture frame device")
             exit()
         if(cv2.waitKey(10)==ord('p')):
             break
@@ -31,7 +41,7 @@ def run(source=0, dispLoc=False):
     points = get_points.run(img) 
 
     if not points:
-        print "ERROR: No object to be tracked."
+        print("ERROR: No object to be tracked.")
         exit()
     
     cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
@@ -43,11 +53,13 @@ def run(source=0, dispLoc=False):
     # Provide the tracker the initial position of the object
     tracker.start_track(img, dlib.rectangle(*points[0]))
 
+    count = 0
+
     while True:
         # Read frame from device or file
         retval, img = cam.read()
         if not retval:
-            print "Cannot capture frame device | CODE TERMINATING :("
+            print("Cannot capture frame device | CODE TERMINATING :(")
             exit()
         # Update the tracker  
         tracker.update(img)
@@ -56,18 +68,40 @@ def run(source=0, dispLoc=False):
         rect = tracker.get_position()
         pt1 = (int(rect.left()), int(rect.top()))
         pt2 = (int(rect.right()), int(rect.bottom()))
+        # pt1new = (int(pt1[0]) + int(pt1[1]))/2
+        # pt2new = (int(pt2[0]) + int(pt2[1]))/2
+        # new1 = pt1new
+        # new2 = pt2new + 20
+        # pttupleleel = (int(new1),int(new2))
         cv2.rectangle(img, pt1, pt2, (255, 255, 255), 3)
-        print "Object tracked at [{}, {}] \r".format(pt1, pt2),
+
+        crop = img[pt1[1]:pt2[1],pt1[0]:pt2[0]]
+        height, width, channels = crop.shape 
+        print(height/2, width/2)
+        # cv2.circle(img, (int(height/2),  int(width/2)),50, (255, 255, 255), 3)
+
+        # todo txt file 
+        cv2.imwrite("extracted/frame%d.jpg" % count, crop)     # save frame as JPEG file
+        count += 1
+
+        cv2.imshow("croped", crop)
+        
+        # cv2.circle(img, (pttupleleel), 100, (255,255,255), 3)
+        # print("Object tracked at [{}, {}] \r".format(pt1, pt2)),
         if dispLoc:
             loc = (int(rect.left()), int(rect.top()-20))
             txt = "Object tracked at [{}, {}]".format(pt1, pt2)
             cv2.putText(img, txt, loc , cv2.FONT_HERSHEY_SIMPLEX, .5, (255,255,255), 1)
+
+        
+     
+
         cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
         cv2.imshow("Image", img)
         # Continue until the user presses ESC key
+    
         if cv2.waitKey(1) == 27:
-            break
-
+                break
     # Relase the VideoCapture object
     cam.release()
 
